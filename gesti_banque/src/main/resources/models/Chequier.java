10a1d3e744740e547494edfb4c129948952d8735
pirato <muchhus@gmail.com>
2020-01-24 12:00:43 +0100

    Correction classe compte
    methodes compte mauvais endroit corrigé

--- gesti_banque/src/main/resources/models/Chequier.java
+++ gesti_banque/src/main/resources/models/Chequier.java
@@ -1,4 +1,46 @@
 
 public class Chequier {
+	
+	
+	//param(s)
+	
+	//Constructor(s)
+	public Notification() {
+		
+	}
+	
+	
+	//method(s)
+	public activateAccount() {
+		
+	}
+	
+	public accountRefuse() {
+		
+	}
+	
+	public accountClosure() {
+		
+	}
+	
+	public accountSetting() {
+		
+	}
+	
+	public accountClose() {
+		
+	}
+	
+	public accountClosureRequest() {
+		
+	}
+	
+	public accountOpeningRequest() {
+		
+	}
+	
+	//getters and setters
+	
+
 
 }
--- gesti_banque/src/main/resources/models/Compte.java
+++ gesti_banque/src/main/resources/models/Compte.java
@@ -1,4 +1,46 @@
 
 public class Compte {
+	
+	
+	//param(s)
+	
+	//Constructor(s)
+	public Notification() {
+		
+	}
+	
+	
+	//method(s)
+	public activateAccount() {
+		
+	}
+	
+	public accountRefuse() {
+		
+	}
+	
+	public accountClosure() {
+		
+	}
+	
+	public accountSetting() {
+		
+	}
+	
+	public accountClose() {
+		
+	}
+	
+	public accountClosureRequest() {
+		
+	}
+	
+	public accountOpeningRequest() {
+		
+	}
+	
+	//getters and setters
+	
+
 
 }
