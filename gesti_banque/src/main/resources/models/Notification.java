c9a5481c475e73b2c4ddf704b3dc242e8ad296f9
Sebastien <muchhus@gmail.com>
2020-01-27 12:34:43 +0100

    Revert "Merge branch 'master' into pirato"
    This reverts commit 543591a39cfe3df57f3d427fb225d4a3bc7f083a.

--- gesti_banque/src/main/resources/models/Notification.java
+++ gesti_banque/src/main/resources/models/Notification.java
@@ -1,27 +1,6 @@
 
 public class Notification {
 	
-	//param(s)
 	
-	//Constructor(s)
-	public Notification() {
-			super();
-	}
-		
-		
-	//method(s)
-	public createNotification() {
-			
-	}
-		
-	public readNotification() {
-			
-	}
-	
-	public deleteNotification() {
-		
-	}
-		
-	//getters and setters	
 
 }
