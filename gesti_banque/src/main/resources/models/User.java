373cf36474fec794b77c83b4c23df83fb0a6abda
karine <karine0805@live.fr>
2020-01-27 10:04:56 +0100

    modif user

--- gesti_banque/src/main/resources/models/User.java
+++ gesti_banque/src/main/resources/models/User.java
@@ -27,9 +27,63 @@ public class User {
 		this.typeOfUser = typeOfUser;
 	}
 
-	
+	public String getUser() {
+		return User;
+	}
 
+	public void setUser(String user) {
+		User = user;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String getTypeOfUser() {
+		return typeOfUser;
+	}
+
+	public void setTypeOfUser(String typeOfUser) {
+		this.typeOfUser = typeOfUser;
+	}
 			
 	//getters and setters
+	
 
 }
